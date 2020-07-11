4145a2a7da2b2ba26f50daea64ec2991f162aab5
Diego Alberto Costa <d.costa1990@outlook.com>
2020-07-11 19:35:11 +0200

    improving the movie cards and connection between the pages

--- src/App.js
+++ src/App.js
@@ -3,36 +3,43 @@ import './App.css';
 import 'bulma/css/bulma.css';
 import { Title }  from './component/title';
 import { SearchForm } from './component/search-form';
+import { MoviesList } from './component/movieList';
+
+import { Detail } from '../src/pages/Details';
 
 
 class App extends Component {
-  state = { results: [] };
+  state = { usedSearch: false, results: [] };
 
   _handleResults = (results) =>{
-    this.setState({ results });
+    this.setState({ results, usedSearch: true });
   }
 
-  _renderResults () {
-    const {results} = this.state
-
-    return results.map(movie => {
-      return <p key={movie.imdbID}>{movie.Title}</p>
-    })
+  _renderResults() {
+    return this.state.results.length === 0 
+      ? <p>Sorry! Results not found!</p> 
+      : <MoviesList movies={this.state.results} />
+    
   }
+
 
   render(){
+    const url = new URL(document.location)
+    const hasId = url.searchParams.has('id')
+
+    if (hasId) {
+      return <Detail id={url.searchParams.get('id')} />
+    }
     return (
       <div className="App">
         <Title>Search your movie</Title>
         <div className="SearchForm-wrapper">
           <SearchForm onResult={this._handleResults} />
         </div>
-        <div>
-          {this.state.results.length === 0 
-            ? <p>Sin Resultados</p> 
-            : this._renderResults()
+        {this.state.usedSearch
+            ? this._renderResults()
+            : <small>Use the form to search a movie</small>
           }
-        </div>
       </div>
     );
   }

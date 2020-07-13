79ac3c2819a0f3fb0c8beae5dd764b934e00cd96
Diego Alberto Costa <d.costa1990@outlook.com>
2020-07-13 12:40:27 +0200

    finishing the Error page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,6 +6,8 @@ export const NotFound = () => (
         <h1 className='title'>Error 404</h1>
         <h2 className='subtitle'>Page not found</h2>
         <br />
+        <h4 className="subtitle">Go back to the home page to continue the Movie Search</h4>
         <ButtonBackToHome />
+        
     </div>
 )
